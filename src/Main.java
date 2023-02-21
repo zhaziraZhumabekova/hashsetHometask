823d8ac230d92301a84c5d17aa58e05ad953684d
Toshiba <zhazira9818@gmail.com>
2023-02-21 22:34:02 +0600

    hashSetHometask

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Set<Integer> set1 = new HashSet<Integer>();
+        Set<Integer> set2 = new HashSet<Integer>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        set2.add(0);
+        set2.add(1);
+        set2.add(2);
+        symSetDiff(set1, set2);
+    }
+    public static void symSetDiff(Set<Integer>set1, Set<Integer>set2){
+        Set<Integer> newSet = new HashSet<Integer>(set1);
+        newSet.removeAll(set2);
+        set2.removeAll((set1));
+        newSet.addAll(set2);
+        System.out.println(newSet);
     }
 }
